5d6e9ae7ab462773b4e24c5fc4262bed96d80326
hatoo <hato2000@gmail.com>
2024-01-24 16:20:08 +0900

    WIP

--- M5Poker.ino
+++ M5Poker.ino
@@ -6,25 +6,87 @@ void setup()
 {
     M5Cardputer.begin();
     sprite.createSprite(M5Cardputer.Display.width(), M5Cardputer.Display.height());
+    // width = 240
+    // height = 135
     sprite.setRotation(1);
 }
 
 void loop()
 {
-    static int counter = 0;
+    static u8_t cards[5] = {0x00, 0x11, 0x22, 0x33, 0x40};
+    static u8_t holds = 0;
     M5Cardputer.update();
-    if (M5Cardputer.BtnA.wasPressed())
+
+    if (M5Cardputer.Keyboard.isChange())
     {
-        counter++;
+        if (M5Cardputer.Keyboard.isKeyPressed('1'))
+        {
+            holds ^= 0x01;
+        }
+
+        if (M5Cardputer.Keyboard.isKeyPressed('2'))
+        {
+            holds ^= 0x02;
+        }
+
+        if (M5Cardputer.Keyboard.isKeyPressed('3'))
+        {
+            holds ^= 0x04;
+        }
+
+        if (M5Cardputer.Keyboard.isKeyPressed('4'))
+        {
+            holds ^= 0x08;
+        }
+
+        if (M5Cardputer.Keyboard.isKeyPressed('5'))
+        {
+            holds ^= 0x10;
+        }
     }
 
     sprite.clear();
-    sprite.setCursor(0, 0);
-    sprite.printf("width = %d, height = %d", M5Cardputer.Display.width(), M5Cardputer.Display.height());
-    sprite.drawLine(0, 0, 240, 135, TFT_WHITE);
-    sprite.drawLine(0, 60, 120, 60, TFT_GREEN);
-    sprite.drawCircle(0, 0, 16, TFT_RED);
+
+    drawCards(cards, holds);
 
     sprite.pushSprite(0, 0);
-    delay(100);
+    delay(40);
+}
+
+void drawCards(u8_t cards[5], u8_t holds)
+{
+    for (int i = 0; i < 5; i++)
+    {
+        drawCard(cards[i], i);
+    }
+
+    sprite.setTextDatum(middle_center);
+    sprite.setTextSize(1.5);
+    for (int i = 0; i < 5; i++)
+    {
+        if (holds & (1 << i))
+        {
+            sprite.drawString("HOLD", 5 + i * (42 + 5) + 21, 10 + 60 + 5);
+        }
+    }
+}
+
+void drawCard(u8_t card, int pos)
+{
+    const char *numbers[13] = {"A", "2", "3", "4", "5", "6",
+                               "7", "8", "9", "10", "J", "Q", "K"};
+    // TODO: Draw graphical suites
+    const char *suits[4] = {"S", "D", "C", "H"};
+
+    const int y = 10;
+    int x = 5 + pos * (42 + 5);
+
+    sprite.drawRect(x, y, 42, 60, TFT_WHITE);
+
+    sprite.setTextDatum(top_left);
+    sprite.setTextSize(2);
+    sprite.drawString(numbers[card >> 4], x + 5, y + 5);
+
+    sprite.setTextDatum(middle_center);
+    sprite.drawString(suits[card & 0x0F], x + 21, y + 35);
 }
