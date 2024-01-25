963b7fb2d0caa259019ada0d68113f077dc7400f
hatoo <hato2000@gmail.com>
2024-01-25 16:01:08 +0900

    wip

--- M5Poker.ino
+++ M5Poker.ino
@@ -1,12 +1,12 @@
 #include <M5Cardputer.h>
 
-const int cardY = 10;
+const int cardY = 30;
 
 LGFX_Sprite sprite = LGFX_Sprite(&M5Cardputer.Display);
 u8_t deck[13 * 4];
 int card_index = 0;
 
-static u8_t hand[5] = {};
+int credit = 100;
 
 void setup()
 {
@@ -20,64 +20,104 @@ void setup()
     {
         deck[i] = i;
     }
-
-    shuffle();
-
-    for (int i = 0; i < 5; i++)
-    {
-        hand[i] = dealCard();
-    }
 }
 
 void loop()
 {
+    static int state = 0;
+    static u8_t hand[5] = {};
     static u8_t holds = 0;
     M5Cardputer.update();
 
-    if (M5Cardputer.Keyboard.isChange())
+    sprite.clear();
+
+    drawInfo();
+
+    switch (state)
     {
-        if (M5Cardputer.Keyboard.isKeyPressed('1'))
+    case 0:
+        // Pre deal
+        for (int i = 0; i < 5; i++)
         {
-            holds ^= 0x01;
+            drawCardBack(i);
         }
 
-        if (M5Cardputer.Keyboard.isKeyPressed('2'))
+        if (M5Cardputer.Keyboard.isChange())
         {
-            holds ^= 0x02;
-        }
-
-        if (M5Cardputer.Keyboard.isKeyPressed('3'))
-        {
-            holds ^= 0x04;
-        }
-
-        if (M5Cardputer.Keyboard.isKeyPressed('4'))
-        {
-            holds ^= 0x08;
-        }
-
-        if (M5Cardputer.Keyboard.isKeyPressed('5'))
-        {
-            holds ^= 0x10;
-        }
-
-        if (M5Cardputer.Keyboard.isKeyPressed(KEY_ENTER))
-        {
-            for (int i = 0; i < 5; i++)
+            if (M5Cardputer.Keyboard.isKeyPressed(KEY_ENTER))
             {
-                if (!(holds & (1 << i)))
+                state = 1;
+                shuffle();
+                for (int i = 0; i < 5; i++)
                 {
                     hand[i] = dealCard();
                 }
+                holds = 0;
             }
-            holds = 0;
-            shuffle();
+        }
+
+        break;
+
+    case 1:
+        drawCards(hand, holds);
+        // Post deal
+        if (M5Cardputer.Keyboard.isChange())
+        {
+            if (M5Cardputer.Keyboard.isKeyPressed('1'))
+            {
+                holds ^= 0x01;
+            }
+
+            if (M5Cardputer.Keyboard.isKeyPressed('2'))
+            {
+                holds ^= 0x02;
+            }
+
+            if (M5Cardputer.Keyboard.isKeyPressed('3'))
+            {
+                holds ^= 0x04;
+            }
+
+            if (M5Cardputer.Keyboard.isKeyPressed('4'))
+            {
+                holds ^= 0x08;
+            }
+
+            if (M5Cardputer.Keyboard.isKeyPressed('5'))
+            {
+                holds ^= 0x10;
+            }
+
+            if (M5Cardputer.Keyboard.isKeyPressed(KEY_ENTER))
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    if (!(holds & (1 << i)))
+                    {
+                        hand[i] = dealCard();
+                    }
+                }
+                holds = 0;
+                state = 2;
+            }
+        }
+        break;
+    case 2:
+        // Post draw
+
+        drawCards(hand, 0);
+        // TODO
+        sprite.setTextDatum(top_left);
+        sprite.drawString("Result", 5, 90);
+
+        if (M5Cardputer.Keyboard.isChange())
+        {
+            if (M5Cardputer.Keyboard.isKeyPressed(KEY_ENTER))
+            {
+                state = 0;
+            }
         }
     }
-
-    sprite.clear();
-
-    drawCards(hand, holds);
 
     sprite.pushSprite(0, 0);
     delay(40);
@@ -116,6 +156,21 @@ void drawCards(u8_t hand[5], u8_t holds)
             sprite.drawString("HOLD", 5 + i * (42 + 5) + 21, cardY + 60 + 5);
         }
     }
+}
+
+void drawInfo()
+{
+    sprite.setTextDatum(top_left);
+    sprite.setTextSize(1.5);
+    sprite.drawString("Jack or better", 5, 5);
+    sprite.drawString("Credit: ", 5, 120);
+    sprite.drawString(String(credit), 68, 120);
+}
+
+void drawCardBack(int pos)
+{
+    int x = 5 + pos * (42 + 5);
+    sprite.drawRect(x, cardY, 42, 60, TFT_WHITE);
 }
 
 void drawCard(u8_t card, int pos)
